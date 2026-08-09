f46d344c33567cca8e56718b3fa521f1c4032686
MKDevTests <ton@email.com>
2026-08-09 07:04:02 +0200

    chore(release): v1.1.4

--- komelia-app/build.gradle.kts
+++ komelia-app/build.gradle.kts
@@ -129,7 +129,7 @@ android {
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
         manifestPlaceholders["appLabel"] = "Korabooks"
-        versionCode = 10103
+        versionCode = 10104
         versionName = libs.versions.app.version.get()
 
         val enableSelfUpdates = when (androidVariant) {
